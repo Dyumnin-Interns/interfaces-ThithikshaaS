ee72ed1d2c391f107c4b65da7bca71616f7f1ef5
Thithikshaa <84781589+ThithikshaaS@users.noreply.github.com>
2025-06-06 19:42:20 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -15,32 +15,3 @@ from cocotb_coverage.coverage import CoverPoint, CoverCross, coverage_db
 import random as rand 
 import constraint  
 
-@CoverPoint("top.x",             
-            xf = lambda x,y:x,             
-            bins=[0,1]) 
-@CoverPoint("top.z",             
-            xf = lambda x,y:y,             
-            bins=[0,1]) 
-@CoverCross("top.cross.xz",             
-            items=['top.x','top.z']) 
-def basic_sample(x,y):     
-    pass   
-
-@CoverPoint("top.wr.wr_addr",             
-            xf = lambda wr_addr,wr_en, wr_data, rd_en, rd_addr: wr_addr,             
-            bins=[4,5]) 
-@CoverPoint("top.wr.wr_data",             
-            xf = lambda wr_addr,wr_en, wr_data, rd_en, rd_addr: wr_data,             
-            bins=[0,1]) 
-@CoverPoint("top.wr.wr_en",             
-            xf = lambda wr_addr,wr_en, wr_data, rd_en, rd_addr: wr_en,             
-            bins=[0,1]) 
-@CoverPoint("top.rd.rd_addr",             
-            xf = lambda wr_addr,wr_en, wr_data, rd_en, rd_addr: rd_addr,             
-            bins=[0,1,2,3]) 
-@CoverPoint("top.rd.rd_en",             
-            xf = lambda wr_addr,wr_en, wr_data, rd_en, rd_addr: rd_en,             
-            bins=[0,1]) 
-@CoverCross("top.cross.wr",             
-            items=['top.wr.wr_addr', 'top.wr.wr_data', 'top.wr.wr_en']              
-            )
